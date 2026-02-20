7a28e0d682155522ff9680723a42039c8e626e24
mikkashub <mikkaxrobinos@gmail.com>
2026-02-19 19:48:13 -0500

    Implement writeMessage(msg) securely

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ recognition.start();
 function onSpeak(event){
     const msg = event.results[0][0].transcript;
 }
+
+// listen to and handle the speech event
+recognition.addEventListener('result', onSpeak);
+
+// see in the DOM what the user has spoken
+function writeMessage(msg){
+    const div = document.createElement('div');
+    div.textContent = 'You said: ';
+    const span = document.createElement('span');
+    span.classList.add('box');
+    span.textContent = msg;
+    msgEl.append(div, span);
+}
